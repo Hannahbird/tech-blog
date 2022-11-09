421d9d6f9dd0b5da4e6491f6b62b66835ad3c194
Hannahbird <hannahbrownson18@gmail.com>
2022-11-09 01:30:18 -0500

    working on public folder and utils

---
+++ public/javascript/add-post.js
@@ -0,0 +1,33 @@
+import { modalAlerts } from './utils.js'
+
+async function newFormHandler(event) {
+    event.preventDefault();
+
+    console.log(event);
+
+    const title = document.querySelector('input[name="post-title"').value;
+    const post_text = document.querySelector('#post-text').value;
+
+    const alert = document.querySelector('#post-modal-alert')
+
+    const response = await fetch('/api/posts', {
+        method: 'POST',
+        body: JSON.stringify({
+            title,
+            post_text
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+
+    if (response.ok) {
+        modalAlerts(alert,'success','Post created!')
+        document.location.replace('/dashboard');
+    }
+    else {
+        modalAlerts(alert, 'error', 'All fields must be completed');
+    }
+}
+
+document.querySelector('#create-post-btn').addEventListener('click', newFormHandler);
---
+++ public/javascript/login.js
@@ -0,0 +1,76 @@
+import { modalAlerts } from './utils.js'
+
+async function signupFormHandler(event) {
+    event.preventDefault();
+    console.log("clicked signup button")
+
+    const username = document.querySelector('#email-login').value.trim();
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    const alert = document.querySelector('#login-modal-alert')
+
+    if (username && email && password) {
+        const response = await fetch('/api/users', {
+            method: 'post',
+            body: JSON.stringify({
+                username,
+                email,
+                password
+            }),
+            headers: {'Content-Type': 'application/json'}
+        })
+        
+        if (response.ok) {
+            modalAlerts(alert,'success','Successful sign-up!')
+
+            document.location.replace('/dashboard')
+            console.log('success');
+        }
+        else
+        {
+            modalAlerts(alert, 'error', response.statusText);
+            console.log(response.status)
+        }
+
+    }
+    else {
+            modalAlerts(alert,'warning','All fields must be completed')
+    }
+}
+
+async function loginFormHandler(event) {
+    event.preventDefault();
+
+    const password = document.querySelector('#password-login').value.trim();
+    const email = document.querySelector('#email-login').value.trim();
+
+    const alert = document.querySelector('#login-modal-alert')
+
+    if (email && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'post',
+            body: JSON.stringify({
+                email,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        })
+        
+        if (response.ok) {
+            modalAlerts(alert,'success','Success!')
+            document.location.replace('/')
+        }
+        else {
+            modalAlerts(alert, 'error', 'invalid');
+        }
+        
+
+    }
+    else {
+        modalAlerts(alert, 'error', 'invalid');
+    }
+}
+
+document.querySelector('#signup-btn').addEventListener('click', signupFormHandler);
+document.querySelector('#login-btn').addEventListener('click', loginFormHandler);
